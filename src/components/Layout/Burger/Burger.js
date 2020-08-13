c7e5d57cfc7fb9ee0263ab2ce4a703ff0838a96c
Benjamin Luke Smith-Gordon <ben.smith.gordon@gmail.com>
2020-08-13 09:12:52 +0100

    Added PropType Validation, Burger CSS.
    (Burger now displays correctly on screen)

--- src/components/Layout/Burger/Burger.js
+++ src/components/Layout/Burger/Burger.js
@@ -1,1 +1,17 @@
-//Some Code
+import React from "react";
+
+import CSS from "./Burger.css";
+import Ingredients from "./BurgerIngredients/Ingredients";
+
+const burger = (props) => {
+  return (
+    <div className={CSS.Burger}>
+      <Ingredients type="bread-top" />
+      <Ingredients type="cheese" />
+      <Ingredients type="meat" />
+      <Ingredients type="bread-bottom" />
+    </div>
+  );
+};
+
+export default burger;
--- src/components/Layout/Burger/BurgerIngredients/Ingredients.js
+++ src/components/Layout/Burger/BurgerIngredients/Ingredients.js
@@ -1,39 +1,46 @@
-import React from "react";
+import React, {Component} from "react";
+import PropTypes from 'prop-types';
 
 import CSS from "./Ingredients.css";
 
-const ingredients = (props) => {
-  let ingredient = null;
+class Ingredients extends Component {
+  render () {
+    let ingredient = null;
 
-  switch (props.type) {
-    case "bread-bottom":
-      ingredient = <div className={CSS.BeadBottom}></div>;
-      break;
-    case "bread-top":
-      ingredient = (
-        <div className={CSS.BreadTop}>
-          <div className={CSS.Seeds1}></div>
-          <div className={CSS.Seeds2}></div>
-        </div>
-      );
-      break;
-    case "meat":
-      ingredient = <div className={CSS.Meat}></div>;
-      break;
-    case "cheese":
-      ingredient = <div className={CSS.Cheese}></div>;
-      break;
-    case "bacon":
-      ingredient = <div className={CSS.Bacon}></div>;
-      break;
-    case "salad":
-      ingredient = <div className={CSS.Salad}></div>;
-      break;
-    default:
-      ingredient = null;
+    switch (this.props.type) {
+      case "bread-bottom":
+        ingredient = <div className={CSS.BreadBottom}></div>;
+        break;
+      case "bread-top":
+        ingredient = (
+          <div className={CSS.BreadTop}>
+            <div className={CSS.Seeds1}></div>
+            <div className={CSS.Seeds2}></div>
+          </div>
+        );
+        break;
+      case "meat":
+        ingredient = <div className={CSS.Meat}></div>;
+        break;
+      case "cheese":
+        ingredient = <div className={CSS.Cheese}></div>;
+        break;
+      case "bacon":
+        ingredient = <div className={CSS.Bacon}></div>;
+        break;
+      case "salad":
+        ingredient = <div className={CSS.Salad}></div>;
+        break;
+      default:
+        ingredient = null;
+    }
+  
+    return ingredient;
   }
+}
 
-  return ingredient;
-};
+Ingredients.propTypes = {
+  type: PropTypes.string.isRequired
+}
 
-export default ingredients;
+export default Ingredients;
