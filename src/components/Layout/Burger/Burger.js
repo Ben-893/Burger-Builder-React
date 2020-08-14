87806d81eb8c566d5aa779bfe96f4e3c5387d3fb
Benjamin Luke Smith-Gordon <ben.smith.gordon@gmail.com>
2020-08-14 14:15:48 +0100

    Added Reduce method to Burger to help calculate the ingredient sum dynamically

--- src/components/Layout/Burger/Burger.js
+++ src/components/Layout/Burger/Burger.js
@@ -4,12 +4,18 @@ import CSS from "./Burger.css";
 import Ingredients from "./BurgerIngredients/Ingredients";
 
 const burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
   .map(ingredientKey => {
     return [...Array(props.ingredients[ingredientKey])].map((_, index) => {
       return <Ingredients key={ingredientKey + index} type={ingredientKey} />;
     });
-  });
+  })
+  .reduce((prevValue, currentValue) => {
+    return prevValue.concat(currentValue)
+  }, []);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
 
   return (
     <div className={CSS.Burger}>
