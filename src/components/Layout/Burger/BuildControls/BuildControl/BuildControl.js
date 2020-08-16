37cc938903f0aac1f0d036fc97b75ab0d8ba0c80
Benjamin Luke Smith-Gordon <ben.smith.gordon@gmail.com>
2020-08-16 14:21:45 +0100

    Added controls and map method to BuildControls component to output multiple BuildControls

--- src/components/Layout/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Layout/Burger/BuildControls/BuildControl/BuildControl.js
@@ -2,7 +2,7 @@ import React from 'react';
 import CSS from './BuildControl.css';
 
 const buildControl = (props) => (
-  <div className={CSS.BuildCOntrol}>
+  <div className={CSS.BuildControl}>
     <div className={CSS.Label}>{props.label}</div>
     <button className={CSS.Less}>Less</button>
     <button className={CSS.More}>More</button>
