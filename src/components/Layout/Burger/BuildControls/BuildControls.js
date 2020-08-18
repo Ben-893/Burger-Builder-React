a4ba42c14c1fc60aed2ffe596659d607ba81f04e
Benjamin Luke Smith-Gordon <ben.smith.gordon@gmail.com>
2020-08-18 19:06:16 +0100

    Created a way to display total price and added an Order Now button

--- src/components/Layout/Burger/BuildControls/BuildControls.js
+++ src/components/Layout/Burger/BuildControls/BuildControls.js
@@ -12,6 +12,7 @@ const controls = [
 
 const buildControls = (props) => (
   <div className={CSS.BuildControls}>
+  <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
     {controls.map((ctrl) => (
       <BuildControl
         key={ctrl.label}
@@ -21,6 +22,9 @@ const buildControls = (props) => (
         disabled={props.disabled[ctrl.type]}
       />
     ))}
+    <button 
+    className={CSS.OrderButton}
+    disabled={!props.purchasable}>Order Now</button>
   </div>
 );
 
