edc175c814834df7e98d5d36d5139eda8c1e929e
Benjamin Luke Smith-Gordon <ben.smith.gordon@gmail.com>
2020-08-24 15:54:30 +0100

    Created Backdrop and Backdrop CSS
    Imported Backdrop into Modal, and created purchaseCancelHandler to hide the backdrop when clicked

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -1,1 +1,9 @@
-//Some code to please Github
+import React from 'react';
+
+import CSS from './Backdrop.css';
+
+const backdrop = (props) => (
+    props.show ? <div className={CSS.Backdrop} onClick={props.clicked}></div> : null
+);
+
+export default backdrop;
