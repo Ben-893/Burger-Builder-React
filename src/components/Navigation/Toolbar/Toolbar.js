b748dff6a15b0fcf2a5562d61da58ba73deef695
Benjamin Luke Smith-Gordon <ben.smith.gordon@gmail.com>
2020-08-25 18:49:51 +0100

    Created reusable Navigation items

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,14 +1,15 @@
 import React from "react";
 
-import CSS from './Toolbar.css';
-import Logo from '../../Logo/Logo';
+import CSS from "./Toolbar.css";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
 
 const toolbar = (props) => (
   <header className={CSS.Toolbar}>
     <div>MENU</div>
     <Logo />
     <nav>
-      ...
+      <NavigationItems />
     </nav>
   </header>
 );
