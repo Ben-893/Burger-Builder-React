fe989df0ddafd05154dc60ab337dd84e5edfc418
Benjamin Luke Smith-Gordon <ben.smith.gordon@gmail.com>
2020-09-04 21:10:12 +0100

    Added Axios, and created Axios configuration file
    Also created an Axios post request to Google Firebase Server with some dummy data, from within BurgerBuilder

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios.orders";
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -77,8 +78,25 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    alert("You continue!");
-  };
+    // alert("You continue!");
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: "Benjamin Smith-Gordon",
+        address: {
+          street: "Teststreet 1",
+          postCode: "NW5 3AN",
+          country: "United Kingdom",
+        },
+        email: "test@test.com",
+      },
+      deliveryMethod: "fastest",
+    };
+    axios.post("/orders.json", order)
+         .then(response => console.log(response))
+         .catch(error => console.log(error));
+  }
 
   render() {
     const disabledInfo = {
