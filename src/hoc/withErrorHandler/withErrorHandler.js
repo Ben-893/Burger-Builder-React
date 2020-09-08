266904d11844f14aa3a5cbba15ef8927e89c229c
Benjamin Luke Smith-Gordon <ben.smith.gordon@gmail.com>
2020-09-08 17:45:38 +0100

    Created a method to remove old interceptors

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -6,36 +6,45 @@ import Auxiliary from "../Auxiliary/Auxiliary";
 const withErrorHandler = (WrappedComponent, axios) => {
   return class extends Component {
     state = {
-      error: null
-    }
+      error: null,
+    };
 
     componentWillMount() {
-      axios.interceptors.request.use(request => {
-        this.setState({error: null});
+      this.requestInterceptor = axios.interceptors.request.use((request) => {
+        this.setState({ error: null });
         return request;
       });
-      axios.interceptors.response.use(response => response, error => {
-        this.setState({error: error});
-      });
+      this.responseInterceptor = axios.interceptors.response.use(
+        (response) => response,
+        (error) => {
+          this.setState({ error: error });
+        }
+      );
+    }
+
+    componentWillUnmount() {
+      axios.interceptors.request.eject(this.requestInterceptor);
+      axios.interceptors.response.eject(this.responseInterceptor);
     }
 
     errorConfirmedHandler = () => {
-      this.setState({error: null});
-    }
+      this.setState({ error: null });
+    };
 
-    render () {
+    render() {
       return (
         <Auxiliary>
-        <Modal 
-          show={this.state.error}
-          modalClosed={this.errorConfirmedHandler}>
-          {this.state.error ? this.state.error.message : null}
-        </Modal>
-        <WrappedComponent {...this.props} />
-      </Auxiliary>
+          <Modal
+            show={this.state.error}
+            modalClosed={this.errorConfirmedHandler}
+          >
+            {this.state.error ? this.state.error.message : null}
+          </Modal>
+          <WrappedComponent {...this.props} />
+        </Auxiliary>
       );
     }
-  }
-}
+  };
+};
 
 export default withErrorHandler;
