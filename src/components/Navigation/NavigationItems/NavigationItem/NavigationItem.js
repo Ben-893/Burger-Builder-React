f2e08ba9cefcb1d5c3a9a85e260e2d1f4c0af25f
Benjamin Luke Smith-Gordon <ben.smith.gordon@gmail.com>
2020-09-13 17:43:42 +0100

    Added a Navigation Link to point to the Orders Page (formally Checkout) and fixed CSS active statuses

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,13 +1,13 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 import CSS from "./NavigationItem.css";
 
 const navigationItem = (props) => (
   <li className={CSS.NavigationItem}>
-    <a href={props.link} 
-       className={props.active ? CSS.active : null}>
+    <NavLink to={props.link} exact={props.exact} activeClassName={CSS.active}>
       {props.children}
-    </a>
+    </NavLink>
   </li>
 );
 
