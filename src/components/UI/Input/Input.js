3207b433541118b67c0b02640063f81c8ec4b720
Benjamin Luke Smith-Gordon <ben.smith.gordon@gmail.com>
2020-09-17 17:40:32 +0100

    Changed the format of the State in ContactData, and the way an Input fields will be structured

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -5,15 +5,33 @@ import CSS from "./Input.css";
 const input = (props) => {
   let inputElement = null;
 
-  switch (props.inputtype) {
+  switch (props.elementType) {
     case "input":
-      inputElement = <input className={CSS.InputElement} {...props} />;
+      inputElement = (
+        <input
+          className={CSS.InputElement}
+          {...props.elementConfig}
+          value={props.value}
+        />
+      );
       break;
     case "textarea":
-      inputElement = <textarea className={CSS.InputElement} {...props} />;
+      inputElement = (
+        <textarea
+          className={CSS.InputElement}
+          {...props.elementConfig}
+          value={props.value}
+        />
+      );
       break;
     default:
-      inputElement = <input className={CSS.InputElement} {...props} />;
+      inputElement = (
+        <input
+          className={CSS.InputElement}
+          {...props.elementConfig}
+          value={props.value}
+        />
+      );
   }
 
   return (
