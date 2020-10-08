221b211e520b9857462c00e62f88b40f5ef80a82
Ben-893 <ben.smith.gordon@gmail.com>
2020-10-08 15:13:26 +0100

    Added the functionality to redirect to the home page after an order is placed

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -2,5 +2,5 @@ export {
   addIngredient,
   removeIngredient,
   initIngredients,
-} from "./burgerBuilder";
-export { purchaseBurger } from "./order";
+} from './burgerBuilder';
+export { purchaseBurger, purchaseInit } from './order';
