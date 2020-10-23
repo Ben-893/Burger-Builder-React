83624415356706884d1a14f3b5f9e5aa4fc30ea4
Ben-893 <ben.smith.gordon@gmail.com>
2020-10-23 12:05:29 +0100

    Created a Logout link to redirect to the home page

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -9,7 +9,11 @@ const navigationItems = props => (
       Burger Builder
     </NavigationItem>
     <NavigationItem link='/orders'>Orders</NavigationItem>
-    {!props.isAuthenticated ? <NavigationItem link='/auth'>Authenticate</NavigationItem> : <NavigationItem link='/logout'>Logout</NavigationItem>}
+    {!props.isAuthenticated ? (
+      <NavigationItem link='/auth'>Authenticate</NavigationItem>
+    ) : (
+      <NavigationItem link='/logout'>Logout</NavigationItem>
+    )}
   </ul>
 );
 
