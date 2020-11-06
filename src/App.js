b6ca057f3c5f67e742cdac51dd73272085692c72
Ben-893 <ben.smith.gordon@gmail.com>
2020-11-06 19:36:46 +0000

    Converted both Order and Checkout containers into functional components

--- src/App.js
+++ src/App.js
@@ -24,33 +24,35 @@ const app = props => {
     props.onTryAutoSignup();
   }, []);
 
-    let routes = (
+  let routes = (
+    <Switch>
+      <Route path='/auth' render={props => <Auth {...props} />} />
+      <Route path='/' exact component={BurgerBuilder} />
+      <Redirect to='/' />
+    </Switch>
+  );
+
+  if (props.isAuthenticated) {
+    routes = (
       <Switch>
-        <Route path='/auth' render={() => <Auth />} />
+        <Route path='/checkout' render={props => <Checkout {...props} />} />
+        <Route path='/orders' render={props => <Orders {...props} />} />
+        <Route path='/logout' component={Logout} />
+        <Route path='/auth' render={props => <Auth {...props} />} />
         <Route path='/' exact component={BurgerBuilder} />
         <Redirect to='/' />
       </Switch>
     );
+  }
 
-    if (props.isAuthenticated) {
-      routes = (
-        <Switch>
-          <Route path='/checkout' render={() => <Checkout />} />
-          <Route path='/orders' render={() => <Orders />} />
-          <Route path='/logout' component={Logout} />
-          <Route path='/auth' render={() => <Auth />} />
-          <Route path='/' exact component={BurgerBuilder} />
-          <Redirect to='/' />
-        </Switch>
-      );
-    }
-
-    return (
-      <div>
-        <Layout><Suspense fallback={<p>Loading...</p>}>{routes}</Suspense></Layout>
-      </div>
-    );
-}
+  return (
+    <div>
+      <Layout>
+        <Suspense fallback={<p>Loading...</p>}>{routes}</Suspense>
+      </Layout>
+    </div>
+  );
+};
 
 const mapStateToProps = state => {
   return {
